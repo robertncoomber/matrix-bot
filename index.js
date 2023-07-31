7b11ac98f8d28b9b00727f2b7ae86e1a3a30c9de
Robert Coomber <robertnc@gmail.com>
2023-07-31 13:14:42 -0700

    added env

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
 import {
-    MatrixClient,
-    SimpleFsStorageProvider,
-    AutojoinRoomsMixin
+  MatrixClient,
+  SimpleFsStorageProvider,
+  AutojoinRoomsMixin,
 } from "matrix-bot-sdk";
-import { handleResponse, startupSequence } from './openai.js';
+import { handleResponse, startupSequence } from "./openai.js";
+import * as dotenv from "dotenv";
 
-const homeserverUrl = "https://matrix-client.matrix.org"; // make sure to update this with your url
-const accessToken = "syt_cmhiMQ_ktCeqppZmFCprXOFmOdY_1f8pwW";
+dotenv.config();
+
+const homeserverUrl = process.env.MATRIX_HOMESERVER; // make sure to update this with your url
+const accessToken = process.env.MATRIX_ACCESS_TOKEN;
 const storage = new SimpleFsStorageProvider("bot.json");
 
 const client = new MatrixClient(homeserverUrl, accessToken, storage);
 AutojoinRoomsMixin.setupOnClient(client);
 
 client.start().then(() => console.log("Client started!"));
-let roomId = "!yidXwcPJbkXvAixVIL:matrix.org";
+let roomId = process.env.ROOM_ID;
 
 client.sendMessage(roomId, {
-    "msgtype": "m.text",
-    "body": "",
+  msgtype: "m.text",
+  body: "",
 });
 
 client.on("room.message", (roomId, event) => {
-    if (!event["content"]) return;
-    const sender = event["sender"];
-    const body = event["content"]["body"];
+  if (!event["content"]) return;
+  const sender = event["sender"];
+  const body = event["content"]["body"];
 
-    let myId = sender.includes("rhb1");
-    if (myId) return;
-    else console.log(sender.replace(":beeper.com", "").replace(":matrix.org"))
+  let myId = sender.includes("rhb1");
+  if (myId) return;
+  else console.log(sender.replace(":beeper.com", "").replace(":matrix.org"));
 
-    processAI(body);
+  processAI(body);
 });
 
 async function processAI(body) {
-    client.setTyping(roomId, true, 5000);
-    let aiResponse = await handleResponse(body);
-    client.setTyping(roomId, false);
-    client.sendMessage(roomId, {
-        "msgtype": "m.text",
-        "body": aiResponse,
-    });
+  client.setTyping(roomId, true, 5000);
+  let aiResponse = await handleResponse(body);
+  client.setTyping(roomId, false);
+  client.sendMessage(roomId, {
+    msgtype: "m.text",
+    body: aiResponse,
+  });
 }
 
 let response = await startupSequence();
 client.sendMessage(roomId, {
-    "msgtype": "m.text",
-    "body": response,
+  msgtype: "m.text",
+  body: response,
 });
--- openai.js
+++ openai.js
@@ -1,64 +1,67 @@
 import chalk from "chalk";
 import PromptSync from "prompt-sync";
 import { Configuration, OpenAIApi } from "openai";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
 const prompt = PromptSync();
 
 const configuration = new Configuration({
-    organization: "org-WHcqwXkvrqu8UeNV76L5rRQQ",
-    apiKey: process.env.OPENAI_API_KEY,
+  organization: process.env.OPENAI_ORGANIZATION,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
-// console.log(chalk.grey(" - type \"save filename\" to save to the directory.\n"))
 
 let model;
 model = "gpt-3.5-turbo-0301"; // try ada while building
 
-let instructions = "You are a helpful ai assistent who is tasked with helping your only user, Robert, with mentally preparing for the day. He is currently trying to get down to 165 lbs. He is currently at 182. He needs to have a caloric deficite of 500 calories each day. You first message to him is going to be a wake up message at 8 am with a motivational message to start his day.";
+let instructions =
+  "You are a helpful ai assistent who is tasked with helping your only user, Robert, with mentally preparing for the day. He is currently trying to get down to 165 lbs. He is currently at 182. He needs to have a caloric deficite of 500 calories each day. You first message to him is going to be a wake up message at 8 am with a motivational message to start his day.";
 let messages = [];
 
-
 export async function startupSequence() {
-    messages.push({ role: "system", content: instructions })
+  messages.push({ role: "system", content: instructions });
 
-    const response = await openai.createChatCompletion({
-        model: model,
-        temperature: 0.888,
-        max_tokens: 1000,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        top_p: 1,
-        messages: messages
-    }, { timeout: 60000 });
+  const response = await openai.createChatCompletion(
+    {
+      model: model,
+      temperature: 0.888,
+      max_tokens: 1000,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      top_p: 1,
+      messages: messages,
+    },
+    { timeout: 60000 }
+  );
 
-    const response_text = response.data.choices[0].message.content.trim();
-    messages.push({ role: "assistant", content: response_text });
-    console.log("\n" + chalk.greenBright("chat: " + response_text + "\n"));
-    return response_text;
+  const response_text = response.data.choices[0].message.content.trim();
+  messages.push({ role: "assistant", content: response_text });
+  console.log("\n" + chalk.greenBright("chat: " + response_text + "\n"));
+  return response_text;
 }
 
-
 export async function handleResponse(userPrompt) {
-    if (userPrompt == "quit") {
-        return;
-    }
+  if (userPrompt == "quit") {
+    return;
+  }
 
-    messages.push({ role: "user", content: userPrompt })
-    const response = await openai.createChatCompletion({
-        model: model,
-        temperature: 0.888,
-        max_tokens: 1000,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        top_p: 1,
-        messages: messages
-    }, { timeout: 60000 });
+  messages.push({ role: "user", content: userPrompt });
+  const response = await openai.createChatCompletion(
+    {
+      model: model,
+      temperature: 0.888,
+      max_tokens: 1000,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      top_p: 1,
+      messages: messages,
+    },
+    { timeout: 60000 }
+  );
 
-    const response_text = response.data.choices[0].message.content.trim();
-    messages.push({ role: "assistant", content: response_text })
-    console.log("\n" + chalk.greenBright("chat: " + response_text + "\n"));
-    return response_text;
+  const response_text = response.data.choices[0].message.content.trim();
+  messages.push({ role: "assistant", content: response_text });
+  console.log("\n" + chalk.greenBright("chat: " + response_text + "\n"));
+  return response_text;
 }
